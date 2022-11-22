459fba721d1a8e7ba8207ea52550a7642662226d
Nathaniel Batausa <nathanielbatausafgo@gmail.com>
2022-11-23 02:09:53 +0800

    vue semifinal

---
+++ src/components/Beelzebub.vue
@@ -0,0 +1,64 @@
+<template>
+   <h1 class="btitle" style="text-align:center">Beelzebub</h1>
+   <img class="bbo" src="https://images6.alphacoders.com/946/946202.jpg" alt="Oga and Baby Beel">
+   <img class="beel" src="https://images2.alphacoders.com/444/444847.png" alt="Beel Fighto">
+   <img class="aoi" src="https://images6.alphacoders.com/581/581082.png" alt="Kunieda">
+</template>
+
+<style>
+    .btitle{
+        color: rgb(36, 189, 25);
+        text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+        position: absolute;
+        top: 8%;
+        left: 44%;
+    }
+
+    .bbo{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        left: 2%;
+        top: 33%;
+        transition: ease-out 0.50s;
+    }
+
+    .bbo:hover{
+        height: 48%;
+        width: 29%;
+        transition: ease-in 0.20s;
+        border: 3px solid green;
+    }
+
+    .beel{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        left: 35%;
+        top: 33%;
+        transition: ease-out 0.50s;
+    }
+
+    .beel:hover{
+        height: 48%;
+        width: 29%;
+        transition: ease-in 0.20s;
+        border: 3px solid green;
+    }
+
+    .aoi{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        right: 2%;
+        top: 33%;
+        transition: ease-out 0.50s;
+    }
+    
+    .aoi:hover{
+        width: 29%;
+        height: 48%;
+        transition: ease-in 0.20s;
+        border: 3px solid green;
+    }
+</style>
---
+++ src/components/FateSeries.vue
@@ -0,0 +1,62 @@
+<template>
+    <h1 class="ftitle" style="text-align:center">Fate Series</h1>
+    <img class="sa" src="https://images7.alphacoders.com/818/818474.jpg" alt="Saber Alter">
+    <img class="hassan" src="https://images6.alphacoders.com/785/785661.jpg" alt="Hassan I">
+    <img class="ka" src="https://images3.alphacoders.com/607/607023.jpg" alt="King Arthur">
+    
+</template>
+
+<style>
+    .ftitle{
+        color: crimson;
+        text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+        position: absolute;
+        top: 8%;
+        left: 44%;
+    }
+
+    .sa{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        left: 2%;
+        top: 33%;
+    }
+
+    .sa:hover{
+        height: 48%;
+        width: 29%;
+        transition: ease-in-out 0.25s;
+        border: 3px solid red;
+    }
+
+    .hassan{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        left: 35%;
+        top: 33%;
+    }
+
+    .hassan:hover{
+        height: 48%;
+        width: 29%;
+        transition: ease-in-out 0.25s;
+        border: 3px solid blue;
+    }
+
+    .ka{
+        height: 50%;
+        width: 30%;
+        position: absolute;
+        right: 2%;
+        top: 33%;
+    }
+
+    .ka:hover{
+        height: 48%;
+        width: 29%;
+        transition: ease-in-out 0.25s;
+        border: 3px solid gold;
+    }
+</style>
